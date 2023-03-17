2479b0fcd3b4f04166c41036dcfed989b34741c5
ratkokostov <ratko.kostov21@gmail.com>
2023-03-17 21:47:06 +0100

    Initial commit

---
+++ src/main/java/com/finki/emtlab/EmtlabApplication.java
@@ -0,0 +1,13 @@
+package com.finki.emtlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finki/emtlab/EmtlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.emtlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
